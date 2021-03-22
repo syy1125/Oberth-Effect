0f533636b6498fbf9e4dbdf314067ba8480b15a6
syy1125 <syy1125@hotmail.com>
2021-03-21 17:13:58 -0700

    Set up vehicle designer framework

---
+++ Assets/Scripts/Designer/BlockButton.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+[RequireComponent(typeof(Image))]
+public class BlockButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	public ColorBlock Colors;
+
+	private BlockPalette _controller;
+	private Image _image;
+	private bool _hover;
+
+	private bool Selected => _controller.SelectedIndex == transform.GetSiblingIndex();
+
+	private void Awake()
+	{
+		_controller = GetComponentInParent<BlockPalette>();
+		_image = GetComponent<Image>();
+		GetComponent<Button>().onClick.AddListener(SelectBlock);
+	}
+
+	private void Start()
+	{
+		_image.CrossFadeColor(Colors.normalColor, 0, true, true);
+	}
+
+
+	public void DisplayBlock(GameObject block)
+	{
+		GameObject instance = Instantiate(block, transform);
+		instance.transform.localScale = new Vector3(40, 40, 1);
+	}
+
+	private void SelectBlock()
+	{
+		_controller.SelectBlockIndex(transform.GetSiblingIndex());
+		_image.CrossFadeColor(Colors.selectedColor, Colors.fadeDuration, true, true);
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		_hover = true;
+		if (!Selected)
+		{
+			_image.CrossFadeColor(Colors.highlightedColor, Colors.fadeDuration, true, true);
+		}
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		_hover = false;
+		if (!Selected)
+		{
+			_image.CrossFadeColor(Colors.normalColor, Colors.fadeDuration, true, true);
+		}
+	}
+
+	public void OnDeselect()
+	{
+		if (!_hover)
+		{
+			_image.CrossFadeColor(Colors.normalColor, Colors.fadeDuration, true, true);
+		}
+	}
+}
---
+++ Assets/Scripts/Designer/BlockPalette.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(GridLayoutGroup))]
+public class BlockPalette : MonoBehaviour
+{
+	public GameObject BlockButtonPrefab;
+	public GameObject[] Blocks;
+
+	public int SelectedIndex { get; private set; }
+
+	private void Start()
+	{
+		SelectedIndex = -1;
+
+		Transform t = transform;
+
+		foreach (GameObject block in Blocks)
+		{
+			GameObject button = Instantiate(BlockButtonPrefab, t);
+			button.GetComponent<BlockButton>().DisplayBlock(block);
+		}
+	}
+
+	public void SelectBlockIndex(int index)
+	{
+		if (index == SelectedIndex) return;
+
+		if (SelectedIndex >= 0)
+		{
+			transform.GetChild(SelectedIndex).GetComponent<BlockButton>().OnDeselect();
+		}
+
+		SelectedIndex = index;
+	}
+}
