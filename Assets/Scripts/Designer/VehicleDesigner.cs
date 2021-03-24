ed107c06934065ac1239c9b18c33421d3aedbf51
syy1125 <syy1125@hotmail.com>
2021-03-23 18:56:10 -0700

    Designer handle rotation

--- Assets/Scripts/Designer/VehicleDesigner.cs
+++ Assets/Scripts/Designer/VehicleDesigner.cs
@@ -1,14 +1,19 @@
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Grid))]
 public class VehicleDesigner : MonoBehaviour
 {
 	public BlockPalette Palette;
+	public InputActionReference Rotate;
 
 	private Camera _mainCamera;
 	private Plane _plane;
 	private Grid _grid;
+
+	private int _rotation;
 
 	private GameObject _preview;
 	private int _prevIndex;
@@ -19,6 +24,11 @@ public class VehicleDesigner : MonoBehaviour
 		_mainCamera = Camera.main;
 		_plane = new Plane(Vector3.back, Vector3.zero);
 		_grid = GetComponent<Grid>();
+	}
+
+	private void OnEnable()
+	{
+		Rotate.action.performed += HandleRotate;
 	}
 
 	private void Update()
@@ -51,6 +61,7 @@ public class VehicleDesigner : MonoBehaviour
 					}
 
 					_preview = Instantiate(Palette.GetSelectedBlock(), transform);
+					_preview.transform.rotation = Quaternion.AngleAxis(_rotation * 90f, Vector3.forward);
 				}
 
 				_preview.transform.position = _grid.GetCellCenterWorld(gridLocation.Value);
@@ -58,6 +69,28 @@ public class VehicleDesigner : MonoBehaviour
 
 			_prevIndex = Palette.SelectedIndex;
 			_prevLocation = gridLocation;
+		}
+	}
+
+	private void OnDisable()
+	{
+		Rotate.action.performed -= HandleRotate;
+	}
+
+	private void HandleRotate(InputAction.CallbackContext context)
+	{
+		if (Keyboard.current.leftShiftKey.ReadValue() > 0)
+		{
+			_rotation = (_rotation + 1) % 4;
+		}
+		else
+		{
+			_rotation = (_rotation + 3) % 4;
+		}
+
+		if (_preview != null)
+		{
+			_preview.transform.rotation = Quaternion.AngleAxis(_rotation * 90f, Vector3.forward);
 		}
 	}
 }
