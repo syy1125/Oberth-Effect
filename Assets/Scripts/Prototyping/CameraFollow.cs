96ae90245adb23849b8c1bf4322dc5c5263b30bf
syy1125 <syy1125@hotmail.com>
2021-03-29 19:07:00 -0700

    Prototype vehicle controls

---
+++ Assets/Scripts/Prototyping/CameraFollow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform Target;
+
+	public float Response;
+	public float DerivativeTime;
+	public float IntegralTime;
+
+	private LinkedList<Vector2> _offsetHistory;
+	private Vector2 _velocity;
+	private Vector2 _integral;
+
+	private void Awake()
+	{
+		_offsetHistory = new LinkedList<Vector2>();
+		_velocity = Vector2.zero;
+		_integral = Vector2.zero;
+	}
+
+	private void FixedUpdate()
+	{
+		var currentPosition = new Vector2(transform.position.x, transform.position.y);
+		var targetPosition = new Vector2(Target.position.x, Target.position.y);
+		Vector2 offset = targetPosition - currentPosition;
+
+		Vector2 derivative = _offsetHistory.Count > 0
+			? (offset - _offsetHistory.Last.Value) / Time.fixedDeltaTime
+			: Vector2.zero;
+
+		_offsetHistory.AddLast(offset);
+		_integral += offset;
+		while (_offsetHistory.Count > 1 && _offsetHistory.Count > IntegralTime / Time.fixedDeltaTime)
+		{
+			_integral -= _offsetHistory.First.Value;
+			_offsetHistory.RemoveFirst();
+		}
+
+		Vector2 timeScaledIntegral = _integral * Time.fixedDeltaTime;
+
+		_velocity += Response * Time.fixedDeltaTime *
+		             (offset + derivative * DerivativeTime + timeScaledIntegral / IntegralTime);
+
+		transform.position += new Vector3(_velocity.x, _velocity.y) * Time.fixedDeltaTime;
+	}
+}
--- Assets/Scripts/Prototyping/VehicleControl.cs
+++ Assets/Scripts/Prototyping/VehicleControl.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class VehicleControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public InputActionReference Move;
+	public float Acceleration;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Rigidbody2D _body;
+	private Camera _mainCamera;
+
+	private void Awake()
+	{
+		_body = GetComponent<Rigidbody2D>();
+		_mainCamera = Camera.main;
+	}
+
+	private void FixedUpdate()
+	{
+		Vector2 mousePosition = _mainCamera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+		Vector2 vehiclePosition = transform.position;
+		float angle = Vector3.SignedAngle(mousePosition - vehiclePosition, Vector3.up, Vector3.back);
+		transform.rotation = Quaternion.Euler(0, 0, angle);
+		
+		var input = Move.action.ReadValue<Vector2>();
+		_body.AddRelativeForce(input * Acceleration);
+	}
 }
