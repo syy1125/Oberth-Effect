0305bc120ff1498b286dfd0b608dab24f37fe1d3
syy1125 <syy1125@hotmail.com>
2021-03-30 15:47:00 -0700

    Add fuel mechanic

--- Assets/Scripts/Prototyping/VehicleControl.cs
+++ Assets/Scripts/Prototyping/VehicleControl.cs
@@ -1,20 +1,24 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class VehicleControl : MonoBehaviour
 {
 	public InputActionReference Move;
 	public float Acceleration;
+	public float FuelRecovery;
 
+	public Image FuelGauge;
+
+	private float _fuel;
 	private Rigidbody2D _body;
 	private Camera _mainCamera;
 
 	private void Awake()
 	{
+		_fuel = 1;
 		_body = GetComponent<Rigidbody2D>();
 		_mainCamera = Camera.main;
 	}
@@ -25,8 +29,19 @@ public class VehicleControl : MonoBehaviour
 		Vector2 vehiclePosition = transform.position;
 		float angle = Vector3.SignedAngle(mousePosition - vehiclePosition, Vector3.up, Vector3.back);
 		transform.rotation = Quaternion.Euler(0, 0, angle);
-		
-		var input = Move.action.ReadValue<Vector2>();
-		_body.AddRelativeForce(input * Acceleration);
+
+		_fuel = Mathf.Min(_fuel + Time.fixedDeltaTime * FuelRecovery, 1f);
+
+		if (_fuel > 0)
+		{
+			var input = Move.action.ReadValue<Vector2>();
+			_body.AddRelativeForce(input * Acceleration);
+			_fuel -= (input.x + input.y) * Time.fixedDeltaTime;
+		}
+	}
+
+	private void Update()
+	{
+		FuelGauge.fillAmount = Mathf.Clamp01(_fuel);
 	}
 }
