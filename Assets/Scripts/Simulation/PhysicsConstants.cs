b9247961769673f63dd0f849d2b8c6c07f92f8ce
syy1125 <syy1125@hotmail.com>
2021-05-29 15:13:04 -0700

    Define length scale and physics constants

---
+++ Assets/Scripts/Simulation/PhysicsConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Syy1125.OberthEffect.Simulation
+{
+public static class PhysicsConstants
+{
+	public static float LengthScale = 100f;
+	public static float LightSpeed = 2997924.58f;
+}
+}
