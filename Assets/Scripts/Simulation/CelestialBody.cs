e0d9dc162ea4b306ecf83e89ea2616fb23d49aaf
syy1125 <syy1125@hotmail.com>
2021-06-23 11:46:47 -0700

    Fix issues with projectiles hitting stars

--- Assets/Scripts/Simulation/CelestialBody.cs
+++ Assets/Scripts/Simulation/CelestialBody.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using Photon.Pun;
+using Syy1125.OberthEffect.WeaponEffect;
 using UnityEngine;
 
 namespace Syy1125.OberthEffect.Simulation
 {
 [RequireComponent(typeof(CircleCollider2D))]
-public class CelestialBody : MonoBehaviour
+public class CelestialBody : MonoBehaviourPun, IDamageable
 {
 	public float Mass;
 
@@ -43,13 +45,34 @@ public class CelestialBody : MonoBehaviour
 	{
 		Vector2 center = transform.position;
 
-		foreach (Rigidbody2D body in _affected)
+		foreach (Rigidbody2D body in _affected.ToArray())
 		{
+			if (body == null)
+			{
+				_affected.Remove(body);
+				continue;
+			}
+
 			Vector2 r = body.worldCenterOfMass - center;
 			// Avoid potential division by zero issue. Note that a black hole has event horizon radius 2M so this bound should line up.
 			float strength = Mass / Mathf.Max(r.sqrMagnitude, 0.5f);
 			body.AddForce(-r.normalized * strength);
 		}
 	}
+
+	public bool IsMine => true;
+	public int OwnerId => photonView.OwnerActorNr;
+	public float Health => float.PositiveInfinity;
+
+	public float GetDamageModifier(float armorPierce, DamageType damageType)
+	{
+		return 0f;
+	}
+
+	public void TakeDamage(float damage)
+	{}
+
+	public void DestroyByDamage()
+	{}
 }
 }
