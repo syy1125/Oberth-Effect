bdd643b88efe935d301f078d87cd489181c50630
syy1125 <syy1125@hotmail.com>
2021-06-28 14:44:49 -0700

    Create layer attributt

---
+++ Assets/Scripts/Editor/PropertyDrawers/UnityLayerAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace Syy1125.OberthEffect.Editor.PropertyDrawers
+{
+public class UnityLayerAttribute : PropertyAttribute
+{}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(UnityLayerAttribute))]
+public class UnityLayerAttributeEditor : PropertyDrawer
+{
+	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	{
+		property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+	}
+}
+#endif
+}
