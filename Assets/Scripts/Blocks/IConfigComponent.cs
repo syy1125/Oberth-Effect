76ccb8fab2d73202b5bde80d1d85087646bf5bd3
syy1125 <syy1125@hotmail.com>
2021-07-06 12:03:20 -0700

    Start work on configurable blocks

---
+++ Assets/Scripts/Blocks/IConfigComponent.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Syy1125.OberthEffect.Blocks
+{
+public interface IConfigComponent
+{
+	JObject ExportConfig();
+
+	void InitDefaultConfig();
+
+	// Note that, when importing, the config might be a partial one.
+	// The component is expected to only update the options specified in the config if that is the case.
+	void ImportConfig(JObject config);
+}
+}
---
+++ Assets/Scripts/Utils/ConfigUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Syy1125.OberthEffect.Utils
+{
+public static class ConfigUtils
+{
+	private static readonly Dictionary<Type, string> KeyCache = new Dictionary<Type, string>();
+
+	public static string GetConfigKey(Type type)
+	{
+		if (!KeyCache.TryGetValue(type, out string value))
+		{
+			var field = type.GetField("CONFIG_KEY", BindingFlags.Public | BindingFlags.Static);
+
+			if (field == null || field.FieldType != typeof(string))
+			{
+				throw new ArgumentException($"Type {type} does not have `CONFIG_KEY` as a `public const string` field");
+			}
+
+			value = (string) field.GetValue(null);
+
+			if (KeyCache.ContainsValue(value))
+			{
+				throw new ArgumentException($"Config key \"{value}\" for {type} already exists on a different type");
+			}
+
+			KeyCache.Add(type, value);
+		}
+
+		return value;
+	}
+}
+}
