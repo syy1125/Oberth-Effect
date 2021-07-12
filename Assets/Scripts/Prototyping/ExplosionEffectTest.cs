59d7a7a9792426185034f7f79dab1c670b299dac
syy1125 <syy1125@hotmail.com>
2021-07-12 12:02:27 -0700

    Create effect for explosions

---
+++ Assets/Scripts/Prototyping/ExplosionEffectTest.cs
@@ -0,0 +1,26 @@
+﻿using Syy1125.OberthEffect.Simulation;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Syy1125.OberthEffect.Prototyping
+{
+public class ExplosionEffectTest : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Mouse.current.leftButton.wasPressedThisFrame)
+		{
+			Vector3 position = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+			position.z = 0f;
+			ExplosionEffectManager.Instance.PlayEffectAt(position, 1f);
+		}
+
+		if (Mouse.current.rightButton.wasPressedThisFrame)
+		{
+			Vector3 position = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+			position.z = 0f;
+			ExplosionEffectManager.Instance.PlayEffectAt(position, 2f);
+		}
+	}
+}
+}
