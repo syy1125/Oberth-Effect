6b9fcd7b893d1d354d2a018df35a7693587553c3
syy1125 <syy1125@hotmail.com>
2021-07-20 14:02:26 -0700

    Create yaml utility for parsing a document AST

---
+++ Assets/Scripts/Spec/Yaml/YamlHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using YamlDotNet.RepresentationModel;
+
+namespace Syy1125.OberthEffect.Spec.Yaml
+{
+public static class YamlHelper
+{
+	public static void MergeMappingNodes(YamlMappingNode source, YamlMappingNode overrides)
+	{
+		foreach (KeyValuePair<YamlNode, YamlNode> entry in overrides.Children)
+		{
+			if (!source.Children.ContainsKey(entry.Key))
+			{
+				source.Children[entry.Key] = entry.Value;
+				continue;
+			}
+
+			YamlNode sourceChild = source.Children[entry.Key];
+			YamlMappingNode sourceChildMap = sourceChild as YamlMappingNode;
+			YamlMappingNode overrideChildMap = entry.Value as YamlMappingNode;
+
+			if (sourceChildMap != null && overrideChildMap != null)
+			{
+				MergeMappingNodes(sourceChildMap, overrideChildMap);
+			}
+			else if (sourceChildMap == null && overrideChildMap == null)
+			{
+				source.Children[entry.Key] = entry.Value;
+			}
+			else
+			{
+				throw new ArgumentException("Cannot merge mapping and non-mapping node");
+			}
+		}
+	}
+}
+}
---
+++ Assets/Scripts/Spec/Yaml/YamlStreamParserAdapter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.RepresentationModel;
+
+namespace Syy1125.OberthEffect.Spec.Yaml
+{
+/// <summary>
+/// Takes a <code>YamlNode</code> and presents it as a parser for typed parsing.
+/// </summary>
+public class YamlStreamParserAdapter : IParser
+{
+	private readonly IEnumerator<ParsingEvent> _enumerator;
+
+	public YamlStreamParserAdapter(YamlNode rootNode)
+	{
+		_enumerator = YamlStreamConverter.ConvertToEventStream(rootNode).GetEnumerator();
+	}
+
+	public ParsingEvent Current => _enumerator.Current;
+
+	public bool MoveNext()
+	{
+		return _enumerator.MoveNext();
+	}
+}
+
+// Basaed on https://dotnetfiddle.net/jaG1i1
+internal static class YamlStreamConverter
+{
+	public static IEnumerable<ParsingEvent> ConvertToEventStream(YamlNode node)
+	{
+		switch (node)
+		{
+			case YamlScalarNode scalar:
+				return ConvertToEventStream(scalar);
+			case YamlSequenceNode sequence:
+				return ConvertToEventStream(sequence);
+			case YamlMappingNode mapping:
+				return ConvertToEventStream(mapping);
+			default:
+				throw new NotSupportedException($"Unsupported node type: {node.GetType().Name}");
+		}
+	}
+
+	private static IEnumerable<ParsingEvent> ConvertToEventStream(YamlScalarNode scalar)
+	{
+		yield return new Scalar(scalar.Anchor, scalar.Tag, scalar.Value, scalar.Style, false, false);
+	}
+
+	private static IEnumerable<ParsingEvent> ConvertToEventStream(YamlSequenceNode sequence)
+	{
+		yield return new SequenceStart(sequence.Anchor, sequence.Tag, false, sequence.Style);
+		foreach (var node in sequence.Children)
+		{
+			foreach (var evt in ConvertToEventStream(node))
+			{
+				yield return evt;
+			}
+		}
+
+		yield return new SequenceEnd();
+	}
+
+	private static IEnumerable<ParsingEvent> ConvertToEventStream(YamlMappingNode mapping)
+	{
+		yield return new MappingStart(mapping.Anchor, mapping.Tag, false, mapping.Style);
+		foreach (var pair in mapping.Children)
+		{
+			foreach (var evt in ConvertToEventStream(pair.Key))
+			{
+				yield return evt;
+			}
+
+			foreach (var evt in ConvertToEventStream(pair.Value))
+			{
+				yield return evt;
+			}
+		}
+
+		yield return new MappingEnd();
+	}
+}
+}
