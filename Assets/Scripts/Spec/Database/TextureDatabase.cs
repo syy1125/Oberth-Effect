1c1da16e52088aa2d8aecf8ff899bc34f63a5868
syy1125 <syy1125@hotmail.com>
2021-08-21 12:11:04 -0700

    Add default line material to texture databse

--- Assets/Scripts/Spec/Database/TextureDatabase.cs
+++ Assets/Scripts/Spec/Database/TextureDatabase.cs
@@ -14,6 +14,7 @@ public class TextureDatabase : MonoBehaviour
 
 	public Material VehicleBlockMaterial;
 	public Material DefaultParticleMaterial;
+	public Material DefaultLineMaterial;
 
 	private void Awake()
 	{
