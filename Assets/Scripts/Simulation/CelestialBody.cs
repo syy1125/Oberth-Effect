bd799e0e5f7a0d8ccd67aa2aac2617561bf4f708
syy1125 <syy1125@hotmail.com>
2021-09-10 11:41:40 -0700

    CelestialBody use PointEffector2D to create gravity

--- Assets/Scripts/Simulation/CelestialBody.cs
+++ Assets/Scripts/Simulation/CelestialBody.cs
@@ -7,59 +7,21 @@ using UnityEngine;
 
 namespace Syy1125.OberthEffect.Simulation
 {
-[RequireComponent(typeof(CircleCollider2D))]
 public class CelestialBody : MonoBehaviourPun, IDamageable
 {
 	public float Mass;
 
-	private List<Rigidbody2D> _affected;
-
-	private void Awake()
-	{
-		_affected = new List<Rigidbody2D>();
-	}
-
 	private void Start()
 	{
-		float radiusLimit = Mathf.Sqrt(Mass / 0.001f);
-		GetComponent<CircleCollider2D>().radius = radiusLimit;
-	}
+		var circleCollider = gameObject.AddComponent<CircleCollider2D>();
+		var pointEffector = gameObject.AddComponent<PointEffector2D>();
+		
+		circleCollider.radius = Mathf.Sqrt(Mass) / 0.001f;
+		circleCollider.isTrigger = true;
+		circleCollider.usedByEffector = true;
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		Rigidbody2D otherBody = other.attachedRigidbody;
-		if (otherBody != null)
-		{
-			_affected.Add(otherBody);
-		}
-	}
-
-	private void OnTriggerExit2D(Collider2D other)
-	{
-		Rigidbody2D otherBody = other.attachedRigidbody;
-		if (otherBody != null)
-		{
-			_affected.Remove(otherBody);
-		}
-	}
-
-	private void FixedUpdate()
-	{
-		Vector2 center = transform.position;
-
-		foreach (Rigidbody2D body in _affected.ToArray())
-		{
-			if (body == null)
-			{
-				_affected.Remove(body);
-				continue;
-			}
-
-			Vector2 r = body.worldCenterOfMass - center;
-			// Avoid potential division by zero issue. Note that a black hole has event horizon radius 2M so this bound should line up.
-			float strength = Mass / Mathf.Max(r.sqrMagnitude, 0.5f);
-			body.AddForce(-r.normalized * strength);
-		}
+		pointEffector.forceMagnitude = -Mass;
+		pointEffector.forceMode = EffectorForceMode2D.InverseSquared;
 	}
 
 	public bool IsMine => true;
