1101a330bc82c2dbc560f04b53b7aaab6532fc45
syy1125 <syy1125@hotmail.com>
2021-09-14 12:04:38 -0700

    Code cleanup

--- Assets/Scripts/Simulation/PhysicsConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Syy1125.OberthEffect.Simulation
-{
-public static class PhysicsConstants
-{
-	public static float LengthScale = 100f;
-	public static float LightSpeed = 2997924.58f;
-}
-}
