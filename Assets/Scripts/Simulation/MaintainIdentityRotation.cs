b0882750ecaade50869de26b8454209155d32109
syy1125 <syy1125@hotmail.com>
2021-09-21 10:40:34 -0700

    Have shipyard health ring maintain identity rotation

---
+++ Assets/Scripts/Simulation/MaintainIdentityRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Syy1125.OberthEffect.Simulation
+{
+public class MaintainIdentityRotation : MonoBehaviour
+{
+	private void Start()
+	{
+		transform.rotation = Quaternion.identity;
+	}
+}
+}
