4d4214558e2cb221094548fb4c1b47a523c43d90
syy1125 <syy1125@hotmail.com>
2021-10-04 11:00:53 -0700

    Set up test drive room better

---
+++ Assets/Scripts/Simulation/TestDriveRoom.cs
@@ -0,0 +1,29 @@
+using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using Syy1125.OberthEffect.Common.Match;
+using Syy1125.OberthEffect.Utils;
+using UnityEngine;
+
+namespace Syy1125.OberthEffect.Simulation
+{
+public class TestDriveRoom : MonoBehaviour
+{
+	private void OnEnable()
+	{
+		PhotonNetwork.OfflineMode = true;
+		PhotonNetwork.CreateRoom(
+			"", new RoomOptions
+			{
+				CustomRoomProperties = new Hashtable { { PropertyKeys.GAME_MODE, GameMode.TestDrive } }
+			}
+		);
+	}
+
+	private void OnDisable()
+	{
+		PhotonNetwork.LeaveRoom();
+		PhotonNetwork.OfflineMode = false;
+	}
+}
+}
