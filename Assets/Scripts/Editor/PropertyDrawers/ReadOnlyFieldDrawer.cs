11a37625f7882fedfe235c4f02ff842fa8749235
syy1125 <syy1125@hotmail.com>
2021-11-24 12:21:58 -0800

    Better organize scripts

---
+++ Assets/Scripts/Editor/PropertyDrawers/ReadOnlyFieldDrawer.cs
@@ -0,0 +1,35 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Syy1125.OberthEffect.Editor.PropertyDrawers
+{
+public class ReadOnlyFieldAttribute : PropertyAttribute
+{}
+
+#if UNITY_EDITOR
+[CustomPropertyDrawer(typeof(ReadOnlyFieldAttribute))]
+public class ReadOnlyFieldDrawer : PropertyDrawer
+{
+	public override float GetPropertyHeight(
+		SerializedProperty property,
+		GUIContent label
+	)
+	{
+		return EditorGUI.GetPropertyHeight(property, label, true);
+	}
+
+	public override void OnGUI(
+		Rect position,
+		SerializedProperty property,
+		GUIContent label
+	)
+	{
+		GUI.enabled = false;
+		EditorGUI.PropertyField(position, property, label, true);
+		GUI.enabled = true;
+	}
+}
+#endif
+}
