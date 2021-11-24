1ec683fe13222d2dc66e9fe741c4929d81186f4e
syy1125 <syy1125@hotmail.com>
2021-11-24 14:21:26 -0800

    Clean up unused code

--- Assets/Scripts/Spec/Block/Resource/FusionGeneratorSpec.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Syy1125.OberthEffect.Spec.Block.Resource
-{
-public class FusionGeneratorSpec
-{
-	public Dictionary<string, float> GenerationRate;
-}
-}
