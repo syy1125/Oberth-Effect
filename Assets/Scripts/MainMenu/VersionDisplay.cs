45a6af4aa8b604820782b9680441872a4c9246ac
syy1125 <syy1125@hotmail.com>
2021-11-27 16:02:24 -0800

    Add version display to main menu

---
+++ Assets/Scripts/MainMenu/VersionDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Syy1125.OberthEffect.MainMenu
+{
+[RequireComponent(typeof(Text))]
+public class VersionDisplay : MonoBehaviour
+{
+	private void Start()
+	{
+		GetComponent<Text>().text = string.Join(
+			"\n",
+			$"Oberth Effect v{Application.version}",
+			$"Unity {Application.unityVersion}"
+		);
+	}
+}
+}
