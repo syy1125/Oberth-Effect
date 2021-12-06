8d6b695889eb88b3a0ba46d9950686b24b92768a
syy1125 <syy1125@hotmail.com>
2021-12-06 13:36:58 -0800

    Create reference frame system

---
+++ Assets/Scripts/Common/ReferenceFrameProvider.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Syy1125.OberthEffect.Common
+{
+public class ReferenceFrameProvider : MonoBehaviour
+{
+	private Rigidbody2D _body;
+	public static readonly ICollection<ReferenceFrameProvider> ReferenceFrames = new HashSet<ReferenceFrameProvider>();
+	public Vector2 PrevPosition { get; private set; }
+	public float PrevRotation { get; private set; }
+
+	private void Awake()
+	{
+		ReferenceFrames.Add(this);
+		_body = GetComponent<Rigidbody2D>();
+	}
+
+	private void Start()
+	{
+		PrevRotation = 0f;
+	}
+
+	private void FixedUpdate()
+	{
+		PrevPosition = transform.position;
+		if (_body != null) PrevRotation = _body.rotation;
+	}
+
+	private void OnDestroy()
+	{
+		ReferenceFrames.Remove(this);
+	}
+
+	public float EstimateMinApproachDistanceDuringFrame(Vector2 start, Vector2 end)
+	{
+		start -= PrevPosition;
+		end -= (Vector2) transform.position;
+
+		// Project start -> origin vector onto start -> end vector and find t, the interpolation factor
+		Vector2 line = end - start;
+		if (Mathf.Approximately(line.sqrMagnitude, 0f)) return start.magnitude;
+		float t = -Vector2.Dot(start, line) / line.sqrMagnitude;
+
+		// If t is within [0,1], the perpendicular line is the shortest. Otherwise, one of the endpoints is the closes.
+		if (t <= 0)
+		{
+			return start.magnitude;
+		}
+		else if (t >= 1)
+		{
+			return end.magnitude;
+		}
+		else
+		{
+			return Vector2.Lerp(start, end, t).magnitude;
+		}
+	}
+
+	public struct RayStep
+	{
+		// T takes on values of [0,1] corresponding to times of "start of frame" to "end of frame".
+		public float T;
+		public Vector2 WorldStart;
+		public Vector2 WorldEnd;
+		public GameObject Parent;
+	}
+
+	public RayStep[] GetRaySteps(Vector2 start, Vector2 end)
+	{
+		GameObject parent = gameObject;
+		Vector2 currentPosition = transform.position;
+		float currentRotation = _body == null ? 0f : _body.rotation;
+
+		// Transform to local position (but not rotated)
+		start -= PrevPosition;
+		end -= currentPosition;
+		Vector2 line = end - start;
+
+		int stepCount = Mathf.Max(Mathf.CeilToInt(line.magnitude), 1);
+		Vector2[] worldPoints = new Vector2[stepCount + 1];
+
+		for (int i = 0; i <= stepCount; i++)
+		{
+			// Unity's rigidbody.rotation doesn't seem to wrap around
+			// (tested up to ~5,000,000 deg, takes a few minutes as the ang vel cap of 18000 to reach)
+			// So we should be able to safely interpolate here.
+			float t = (float) i / stepCount;
+			worldPoints[i] = RotateVector(
+				Vector2.Lerp(start, end, t) + currentPosition,
+				Mathf.Lerp(PrevRotation, currentRotation, t)
+			);
+		}
+
+		RayStep[] steps = new RayStep[stepCount];
+		for (int i = 0; i < stepCount; i++)
+		{
+			steps[i] = new RayStep
+			{
+				T = (float) i / stepCount,
+				WorldStart = worldPoints[i],
+				WorldEnd = worldPoints[i + 1],
+				Parent = parent
+			};
+		}
+
+		return steps;
+	}
+
+	private Vector2 RotateVector(Vector2 v, float rotation)
+	{
+		float rad = rotation * Mathf.Deg2Rad;
+		float sin = Mathf.Sin(rad), cos = Mathf.Cos(rad);
+
+		return new Vector2(v.x * cos - v.y * sin, v.x * sin + v.y * cos);
+	}
+}
+}
---
+++ Assets/Scripts/Prototyping/ColliderTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using Syy1125.OberthEffect.Common;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Syy1125.OberthEffect.Prototyping
+{
+[RequireComponent(typeof(Rigidbody2D))]
+public class ColliderTest : MonoBehaviour
+{
+	public bool EstimateDistance;
+	public Vector2 TargetVelocity;
+
+	private void Start()
+	{
+		if (EstimateDistance)
+		{
+			StartCoroutine(LateFixedUpdate());
+		}
+	}
+
+	private void Update()
+	{
+		// if (Keyboard.current.spaceKey.wasPressedThisFrame)
+		// {
+		// 	GetComponent<Rigidbody2D>().velocity = TargetVelocity;
+		// }
+	}
+
+	private void FixedUpdate()
+	{
+		var body = GetComponent<Rigidbody2D>();
+
+		if (Keyboard.current.spaceKey.isPressed)
+		{
+			body.AddTorque(1000 * body.inertia * Mathf.Deg2Rad);
+		}
+
+		Debug.Log(body.rotation);
+	}
+
+	private IEnumerator LateFixedUpdate()
+	{
+		Vector2 prevPosition = transform.position;
+		yield return new WaitForFixedUpdate();
+
+		while (enabled)
+		{
+			foreach (var referenceFrame in ReferenceFrameProvider.ReferenceFrames)
+			{
+				Debug.Log(
+					$"Estimated min distance to {referenceFrame.name} is {referenceFrame.EstimateMinApproachDistanceDuringFrame(prevPosition, transform.position)}"
+				);
+			}
+
+			prevPosition = transform.position;
+			yield return new WaitForFixedUpdate();
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log($"Collision with {other.name}");
+		Debug.Log($"Trigger position {transform.position}");
+		Debug.Log($"Other position {other.transform.position}");
+
+		var referenceFrame = other.GetComponent<ReferenceFrameProvider>();
+		if (referenceFrame != null)
+		{
+			Debug.Log($"{other.name} prev position {referenceFrame.PrevPosition}");
+		}
+	}
+}
+}
