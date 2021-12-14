32998a4d91d6ea571b944a72dd2bacb090d47e76
syy1125 <syy1125@hotmail.com>
2021-12-14 11:09:45 -0800

    Update assembly names

--- Assets/Scripts/Designer/Config/DesignerConfig.cs
+++ Assets/Scripts/Designer/Config/DesignerConfig.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Syy1125.OberthEffect.Blocks.Config;
 using Syy1125.OberthEffect.Common;
