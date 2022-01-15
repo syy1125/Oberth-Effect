0b731365ff5a729ff97645397e00b8def638e22a
syy1125 <syy1125@hotmail.com>
2022-01-15 12:22:29 -0800

    Add switch to change unit mode to options screen

--- Assets/Scripts/MainMenu/Options/OptionsScreen.cs
+++ Assets/Scripts/MainMenu/Options/OptionsScreen.cs
@@ -1,4 +1,5 @@
 ﻿using Syy1125.OberthEffect.Common;
+using Syy1125.OberthEffect.Common.Utils;
 using Syy1125.OberthEffect.Components.UserInterface;
 using UnityEngine;
 
@@ -6,13 +7,22 @@ namespace Syy1125.OberthEffect.MainMenu.Options
 {
 public class OptionsScreen : MonoBehaviour
 {
+	public SwitchSelect UnitModeSwitch;
 	public PercentageSlider DesignerGridOpacitySlider;
 
 	private void Start()
 	{
+		UnitModeSwitch.Value = (int) PhysicsUnitUtils.UnitMode;
+		UnitModeSwitch.OnValueChanged.AddListener(HandleUnitModeChange);
+
 		float gridOpacity = PlayerPrefs.GetFloat(PropertyKeys.DESIGNER_GRID_OPACITY, 0.2f);
 		DesignerGridOpacitySlider.Value = gridOpacity;
 		DesignerGridOpacitySlider.OnChange.AddListener(HandleGridOpacityChange);
+	}
+
+	private void HandleUnitModeChange(int unitMode)
+	{
+		PhysicsUnitUtils.SetPhysicsUnitMode((PhysicsUnitMode) unitMode);
 	}
 
 	private void HandleGridOpacityChange(float opacity)
